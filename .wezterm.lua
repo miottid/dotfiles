8e07ca8954ef39b42736b29982a27bca9166a443
David Miotti <david@station-coding.com>
2024-11-01 11:21:09 +0100

    wezterm: increase font size

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,7 +8,7 @@ config.audible_bell = "Disabled"
 
 -- This is where you actually apply your config choices
 config.font = wezterm.font("Iosevka Nerd Font Mono")
-config.font_size = 14
+config.font_size = 16
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.90
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
