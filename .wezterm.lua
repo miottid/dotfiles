4768f6bc79861f3f279b13d05916df3f6da48163
David Miotti <david@station-coding.com>
2025-04-11 08:44:40 +0200

    wezterm: update config

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,13 +10,13 @@ config.audible_bell = "Disabled"
 config.font = wezterm.font("Iosevka Nerd Font Mono")
 config.font_size = 18
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.90
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.initial_rows = 45
 config.initial_cols = 170
 config.window_close_confirmation = "NeverPrompt"
 
--- Configure Leader key
+-- Configure Leader key and layout to mimic tmux
 -- config.leader = { key = 'q', mods = 'CTRL', timeout_milliseconds = 1000 }
 -- config.keys = {
 --     {
