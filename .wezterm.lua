200373ddafb10e765fce064c062322c2e662f19d
David Miotti <david@station-coding.com>
2025-06-06 12:19:55 +0200

    Update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,7 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local mux = wezterm.mux
+local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -15,6 +17,7 @@ config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.initial_rows = 45
 config.initial_cols = 170
 config.window_close_confirmation = "NeverPrompt"
+-- config.window_decorations = "NONE"
 
 -- Configure Leader key and layout to mimic tmux
 -- config.leader = { key = 'q', mods = 'CTRL', timeout_milliseconds = 1000 }
@@ -84,22 +87,30 @@ config.colors = {
 	},
 }
 
--- Start wezterm in the center of screen
--- wezterm.on("gui-startup", function(cmd)
--- 	local screen = wezterm.gui.screens().active
--- 	local ratio = 0.5
--- 	local width, height = screen.width * ratio, screen.height * ratio
-    
--- 	local tab, pane, window = wezterm.mux.spawn_window(cmd or {
--- 		position = {
---             x = (screen.width - width) / 2,
---             y = (screen.height - height) / 2,
---             origin = 'ActiveScreen',
---         },
--- 	})
+local scratch = "_scratch" -- Keep this consisten with Hammerspoon
+wezterm.on("gui-attached", function(domain)
+                               local workspace = mux.get_active_workspace()
+                               if workspace ~= scratch then return end
 
--- 	window:gui_window():set_inner_size(width, height)
--- end)
+                               local screen = wezterm.gui.screens().active
+                               local ratio = 0.5
+                               local width, height = screen.width * ratio, screen.height * ratio
+                               local tab, pane, window = wezterm.mux.spawn_window(cmd or {
+                            	   position = {
+                                       x = (screen.width - width) / 2,
+                                       y = (screen.height - height) / 2,
+                                       origin = 'ActiveScreen',
+                                   },
+                               })
+                               window:gui_window():set_inner_size(width, height)
 
--- and finally, return the configuration to wezterm
+
+                               for _, window in ipairs(mux.all_windows()) do
+                                   local gwin = window:gui_window()
+                                   if gwin ~= nil then
+                                       gwin:perform_action(act.SetWindowLevel "AlwaysOnTop", wgwin:active_pane())
+                                   end
+                               end
+end)
+
 return config
