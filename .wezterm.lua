9263b1ca6781ed93cd80fc415c9578e1e9cc7d16
David Miotti <david@station-coding.com>
2026-03-26 22:33:45 +0100

    feat: switch to Myna font and reduce size to 14

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,9 +9,9 @@ local config = wezterm.config_builder()
 config.audible_bell = "Disabled"
 
 -- This is where you actually apply your config choices
-config.font = wezterm.font("Iosevka Nerd Font Mono")
--- config.font = wezterm.font("Myna")
-config.font_size = 16
+-- config.font = wezterm.font("Iosevka Nerd Font Mono")
+config.font = wezterm.font("Myna")
+config.font_size = 14
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.95
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
@@ -81,31 +81,31 @@ config.window_close_confirmation = "NeverPrompt"
 
 config.color_scheme = "Dark Pastel"
 
-config.colors = {
-	foreground = "#ffffff",
-	background = "#181818",
+-- config.colors = {
+-- 	foreground = "#ffffff",
+-- 	background = "#181818",
 
-	ansi = {
-		"#000000",
-		"#f43841",
-		"#73c936",
-		"#ffdd33",
-		"#5f627f",
-		"#9e95c7",
-		"#5f627f",
-		"#ffffff",
-	},
-	brights = {
-		"#ffffff",
-		"#196a6c",
-		"#73c936",
-		"#ffdd33",
-		"#96a6c8",
-		"#9e95c7",
-		"#96a6c8",
-		"#ffffff",
-	},
-}
+-- 	ansi = {
+-- 		"#000000",
+-- 		"#f43841",
+-- 		"#73c936",
+-- 		"#ffdd33",
+-- 		"#5f627f",
+-- 		"#9e95c7",
+-- 		"#5f627f",
+-- 		"#ffffff",
+-- 	},
+-- 	brights = {
+-- 		"#ffffff",
+-- 		"#196a6c",
+-- 		"#73c936",
+-- 		"#ffdd33",
+-- 		"#96a6c8",
+-- 		"#9e95c7",
+-- 		"#96a6c8",
+-- 		"#ffffff",
+-- 	},
+-- }
 
 local scratch = "_scratch" -- Keep this consisten with Hammerspoon
 wezterm.on("gui-attached", function(domain)
